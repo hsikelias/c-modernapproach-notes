dc6587e9c70f9999236e2212084830820e758770
hsikelias <plekishsai@gmail.com>
2026-08-05 17:43:28 -0400

    daily commit

--- 03.C_FormattedIO/Projects/05.c
+++ 03.C_FormattedIO/Projects/05.c
@@ -10,7 +10,7 @@ int main(void){
     printf("Enter the numbers from 1 to 16 in any order:");
     scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a10,&a11,&a12,&a13,&a14,&a15,&a16);
 
-    printf("%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16);
+    printf("%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16);
                 
     row_sum1 = a1 + a2 +a3 + a4;
     row_sum2 = a5 + a6 + a7 + a8;
@@ -21,10 +21,13 @@ int main(void){
     column_sum2 = a2 + a6 + a10 + a14;
     column_sum3 = a3 + a7 + a11 + a15;
     column_sum4 = a4 + a8 + a12 + a16; 
-
-
+    
+    diagonal_sum1 = a1 + a6 + a11 + a16;
+    diagonal_sum2 = a13 + a10 + a7 + a4;
     
 
-    printf("Row sums: %d %d %d %d",row_sum1, row_sum2, row_sum3, row_sum4);
+    printf("Row sums: %d %d %d %d\n",row_sum1, row_sum2, row_sum3, row_sum4);
+    printf("Column sums: %d %d %d %d\n",column_sum1, column_sum2, column_sum3, column_sum4);
+    printf("Diagonal sums: %d %d",diagonal_sum1, diagonal_sum2);
     return 0;    
 }
--- 03.C_FormattedIO/Projects/06.c
+++ 03.C_FormattedIO/Projects/06.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+int main (void){
+    int num1,denom1,num2,denom2;
+    printf("Enter two fractions separated by a plus sign: ");
+    scanf("%d / %d + %d / %d",&num1, &denom1, &num2, &denom2);
+
+    printf("The sum is %d/%d",num1*denom2+denom1*num2,denom1*denom2);
+
+    return 0;
+}
