51a84f9ceaf7af9eb45d847f6e09adc517b79ead
Lekish Sai Podili <plekishsai@gmail.com>
2026-08-10 17:34:40 -0400

    Gained better understanding of how m and p formating characters work together. Also my previous confusions with how p behaves with digits vs decimals and how scanf behaves with whitespace characters in different scenario

--- 03.C_FormattedIO/tests.c
+++ 03.C_FormattedIO/tests.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 
-// TOOK HANDWRITTEN NOTES + EXPERIMENTING HERE 
 
 int main (void){
-    int num = 53;
+/*
+    int num = 5;
     float num2 = 23;
+    int i,j;
 
-    printf("%-2.2dhi",num);
+    printf("integer: %3.2d\nfloat: %8.4f\n",num,num2);
+    scanf("%dm%d",&i,&j);
     
+ 
+    printf("%d,%d",i,j);
+    */
+
+    int num, denom;
+
+    printf("Enter fraction: ");
+    scanf("%d /%d",&num,&denom);
+    printf("%d/%d",num,denom);
 }
