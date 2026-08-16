a9584472159c5f4ca3108293289a2283b14e9a82
hsikelias <plekishsai@gmail.com>
2026-08-16 12:22:15 -0400

    Completed question 1-3 in chapter 4 exercise

--- 04.C_Expressions/Ch04_exercise.c
+++ 04.C_Expressions/Ch04_exercise.c
@@ -1,3 +1,27 @@
-# Expressions
+// Expressions
+#include <stdio.h>
+
+int main(void){
+    
+    int i, j, k;
+    i = 5;
+    j = 3;
+
+    printf("%d %d\n", i/j, i%j);
+    // output: 1, 2 
+
+    printf("%d\n", (i + 10) % j);  
+    // output: 0
+    
+    k = 9; j = 8; i = 7;
+    printf("%d\n", (i+10) % k / j);
+    // output: 2
+
+    i = 1; j = 2; k =3;
+    printf("%d\n", (i + 5) % (j + 2) / k);
+    // output: 0
+
+    return 0;
+}
 
 
